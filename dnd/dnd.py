2b02c882e639159e7691af711a3c8ea38e4c1016
dougthor42 <dougthor42@users.noreply.github.com>
2014-09-22 17:03:27 -0700

    Starting to understand Tkinter UI stuff. Much easier than C#...

--- dnd/dnd.py
+++ dnd/dnd.py
@@ -17,36 +17,73 @@ Options:
 from __future__ import (print_function, division,
                         absolute_import, unicode_literals)
 from docopt import docopt
-from Tkinter import Tk, BOTH
-from ttk import Frame, Button, Style
+import Tkinter
 
 __author__ = "Douglas Thor"
 __version__ = "v0.1.0"
 
 
-class Example(Frame):
+class Example(Tkinter.Frame):
     def __init__(self, parent):
-        Frame.__init__(self, parent)
+        Tkinter.Frame.__init__(self, parent)
         self.parent = parent
         self.initUI()
 
     def initUI(self):
         self.parent.title("Simple")
-        self.style = Style()
-        self.style.theme_use("default")
 
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill=Tkinter.BOTH, expand=1)
 
-        quitButton = Button(self, text="Quit",
-                            command=self.quit)
+        quitButton = Tkinter.Button(self, text="Quit",
+                                    command=self.buttonClick)
         quitButton.place(x=50, y=50)
+        
+        self.var = Tkinter.IntVar()
+        check_button = Tkinter.Checkbutton(self, text='hellp',
+                                           variable=self.var,
+                                           command=self.onClick)
+
+        check_button.select()
+        check_button.place(x=15, y=15)
+
+        classes = ['A',
+                   'B',
+                   'C',
+                   ]
+
+        list_box = Tkinter.Listbox()
+        for _i in classes:
+            list_box.insert(Tkinter.END, _i)
+
+        list_box.bind("<<ListboxSelect>>", self.onSelect)
+        list_box.place(x=120, y=50)
+
+        self.string_var = Tkinter.StringVar()
+        self.label = Tkinter.Label(self, text=0, textvariable=self.string_var)
+        self.label.place(x=120, y=210)
+
+    def onClick(self):
+        if self.var.get() == 1:
+            self.master.title("Checkbutton")
+        else:
+            self.master.title("")
+
+    def buttonClick(self):
+        print("clicked!")
+
+    def onSelect(self, val):
+        sender = val.widget
+        idx = sender.curselection()
+        value = sender.get(idx)
+
+        self.string_var.set(value)
 
 
 def main():
     """ Main Code """
     docopt(__doc__, version=__version__)
-    root = Tk()
-    root.geometry("250x150+300+300")
+    root = Tkinter.Tk()
+    root.geometry("450x350+300+300")
     app = Example(root)
     root.mainloop()
 
